217bf03d973c2aaf1ac752d78b64f566ec798e4c
Kalin-Rudnicki <deaddorks2@gmail.com>
2020-05-13 06:37:52 -0500

    Reading chars from Iterator

--- src/main/scala/slyce/lexer/dfa/FileReader.scala
+++ src/main/scala/slyce/lexer/dfa/FileReader.scala
@@ -1,40 +1,110 @@
 package slyce.lexer.dfa
 
+import scalaz.std.option._
 import scalaz.std.option.optionSyntax._
-
 import slyce.tree.GeneralToken.Stats.{GlobalSpan, RelativePos}
 
-class FileReader(val lines: Iterator[Char]) {
+import scala.annotation.tailrec
+
+final class FileReader(val source: Iterator[Char]) {
   
-  private var current: RelativePos = RelativePos(0, 0)
-  private var spanStart: Int = 0
-  private var start: RelativePos = current
+  import FileReader._
   
-  private var queue: Option[Char] = None
+  type Group =
+    (Char, CharSpan, LocInFile)
   
-  // TODO (KR) : I am not sure if the '\n' handling happens automatically
-  def getC: Option[Char] =
-    queue match {
-      case Some(c) =>
-        queue = None
-        c.some
-      case None =>
-        lines.nextOption match {
-          case None =>
-            None
-          case Some('\r') =>
-            lines.nextOption match {
-              case None =>
-                '\r'.some
-              case Some('\n') =>
-                '\n'.some
-              case Some(c2) =>
-                queue = c2.some
-                '\r'.some
-            }
-          case Some(c) =>
-            c.some
-        }
-    }
+  private var relative: RelativePos = RelativePos(0, 0)
+  private var global: Int = 0
+  
+  private var queue: List[Either[Char, Group]] = Nil
+  
+  def getC: Option[Group] = {
+    def getFromQueue: Option[Either[Char, Group]] =
+      queue match {
+        case Nil =>
+          None
+        case head :: tail =>
+          queue = tail
+          head.some
+      }
+    
+    def getFromSource: Option[Char] =
+      source.nextOption
+    
+    def nextRaw: Option[Either[Char, Group]] =
+      getFromQueue.cata(
+        fQ => fQ.some,
+        getFromSource.map(Left(_))
+      )
+    
+    def nextOnlyChar: Option[Char] =
+      queue match {
+        case Left(c) :: tail =>
+          queue = tail
+          c.some
+        case Right(_) :: _ =>
+          None
+        case Nil =>
+          getFromSource
+      }
+    
+    def nextSmart: Option[Group] =
+      nextRaw.map {
+        case Left('\r') =>
+          nextOnlyChar match {
+            case None =>
+              newLine
+            case Some('\n') =>
+              newLine
+            case Some(c2) =>
+              queue = Left(c2) :: queue
+              newLine
+          }
+        case Left(c) =>
+          nextInLine(c)
+        case Right(cgr) =>
+          cgr
+      }
+    
+    nextSmart
+  }
+  
+  def nextInLine(c: Char): Group = {
+    val cs = CharSpan(global, global)
+    val res = LocInFile(relative.lineNo, CharSpan(relative.posInLine, relative.posInLine))
+    global += 1
+    relative = relative.nextInLine
+    (c, cs, res)
+  }
+  
+  def newLine: Group = {
+    val cs = CharSpan(global, global)
+    val res = LocInFile(relative.lineNo, CharSpan(relative.posInLine, relative.posInLine))
+    global += 1
+    relative = relative.newLine
+    ('\n', cs, res)
+  }
+  
+  def newLine2: Group = {
+    val cs = CharSpan(global, global + 1)
+    val res = LocInFile(relative.lineNo, CharSpan(relative.posInLine, relative.posInLine + 1))
+    global += 2
+    relative = relative.newLine
+    ('\n', cs, res)
+  }
+  
+  /**
+   * @param list "Group"'s you want to give back, in the order you want to receive them
+   */
+  def pushBack(list: List[Group]): Unit =
+    queue = list.map(Right(_)) ::: queue
+  
+}
+
+object FileReader {
+  
+  final case class CharSpan(start: Int, end: Int)
+  
+  final case class LocInFile(lineNo: Int, charSpanInLine: CharSpan)
   
 }
--- src/main/scala/slyce/tree/GeneralToken.scala
+++ src/main/scala/slyce/tree/GeneralToken.scala
@@ -13,10 +13,18 @@ object GeneralToken {
   final case class Stats(text: String, span: GlobalSpan, start: RelativePos, end: RelativePos)
   
   object Stats {
-  
+    
     final case class GlobalSpan(start: Int, end: Int)
   
-    final case class RelativePos(lineNo: Int, posInLine: Int)
+    final case class RelativePos(lineNo: Int, posInLine: Int) {
+      
+      def nextInLine: RelativePos =
+        this.copy(lineNo = lineNo, posInLine = posInLine + 1)
+      
+      def newLine: RelativePos =
+        this.copy(lineNo = lineNo + 1, posInLine = 0)
+      
+    }
     
   }
   
