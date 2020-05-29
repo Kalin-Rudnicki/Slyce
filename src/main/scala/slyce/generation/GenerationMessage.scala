e01d9bb208dce74f9d921768dc185f7f4263f055
Kalin-Rudnicki <deaddorks@gmail.com>
2020-05-29 05:35:29 -0500

    SLYCE-19 Set up FatalError

--- src/main/scala/slyce/generation/GenerationMessage.scala
+++ src/main/scala/slyce/generation/GenerationMessage.scala
@@ -19,52 +19,74 @@ object GenerationMessage {
 
   // =====| Fatal |=====
 
-  case class FatalError(message: String)(implicit val pos: Position) extends GenerationMessage
+  object Fatal {
 
-  object FatalError {
+    trait Message extends GenerationMessage
 
-    def canNotJoinEmptyMaps(implicit pos: Position): GenerationMessage =
-      FatalError("Can not join empty set of transition maps")
+    case class BasicMessage(message: String)(implicit val pos: Position) extends Message
 
-    def noSuchModeToTransitionTo(lineNo: Int, modeName: String)(implicit pos: Position): GenerationMessage =
-      FatalError(s"Unable to transition to non-existent mode '$modeName' from regex on line #$lineNo")
+    def canNotJoinEmptyMaps(implicit pos: Position): Message =
+      BasicMessage("Can not join empty set of transition maps")
 
-    def emptyStringCompletesRegex(lineNo: Int)(implicit pos: Position): GenerationMessage =
-      FatalError(s"Regex on line #$lineNo can be satisfied with no input")
+    case class NoSuchModeToTransitionTo(lineNo: Int, modeName: String)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Unable to transition to non-existent mode '$modeName' from regex on line #$lineNo"
+    }
+
+    case class EmptyStringCompletesRegex(lineNo: Int)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Regex on line #$lineNo can be satisfied with no input"
+    }
 
     // TODO (KR) : lineNo?
-    def repeatMinNegative(min: Int)(implicit pos: Position): GenerationMessage =
-      FatalError(s"Tried to repeat a regex a negative ($min) amount of times")
+    case class RepeatMinNegative(min: Int)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Tried to repeat a regex a negative ($min) amount of times"
+    }
 
-    def repeatMaxNonPositive(max: Int)(implicit pos: Position): GenerationMessage =
-      FatalError(s"Tried to repeat a regex a non-positive ($max) amount of times")
+    // TODO (KR) : lineNo?
+    case class RepeatMaxNonPositive(max: Int)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Tried to repeat a regex a non-positive ($max) amount of times"
+    }
 
-    def repeatMaxMin(min: Int, max: Int)(implicit pos: Position): GenerationMessage =
-      FatalError(s"Tried to repeat a regex where max times ($max) was less than min times ($min)")
+    // TODO (KR) : lineNo?
+    case class RepeatMaxMin(min: Int, max: Int)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Tried to repeat a regex where max times ($max) was less than min times ($min)"
+    }
 
-    def badModeName(name: String)(implicit pos: Position): GenerationMessage =
-      FatalError(s"Bad mode name: '$name'")
-
+    // TODO (KR) : lineNo?
+    case class BadModeName(name: String)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Bad mode name: '$name'"
+    }
   }
 
   // =====| NonFatal |=====
 
-  case class GenericWarning(message: String)(implicit val pos: Position) extends GenerationMessage
+  object NonFatal {
 
-  case class DuplicateModeIgnored(modeName: String)(implicit val pos: Position) extends GenerationMessage {
-    override def message: String =
-      s"Mode $modeName was duplicated, and will be ignored"
-  }
+    trait Message extends GenerationMessage
 
-  case class CompletelyShadowedRegex(lineNo: Int, shadowingLines: List[Int])(implicit val pos: Position)
-      extends GenerationMessage {
-    override def message: String =
-      s"Regex on line #$lineNo is completely shadowed by regexes on lines: ${shadowingLines.mkString(", ")}"
-  }
+    case class BasicMessage(message: String)(implicit val pos: Position) extends Message
 
-  case class InaccessibleNfaState(mode: String, stateNo: Int)(implicit val pos: Position) extends GenerationMessage {
-    override def message: String =
-      s"State #$stateNo in mode $mode is inaccessible"
+    case class DuplicateModeIgnored(modeName: String)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Mode $modeName was duplicated, and will be ignored"
+    }
+
+    case class CompletelyShadowedRegex(lineNo: Int, shadowingLines: List[Int])(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"Regex on line #$lineNo is completely shadowed by regexes on lines: ${shadowingLines.mkString(", ")}"
+    }
+
+    case class InaccessibleNfaState(mode: String, stateNo: Int)(implicit val pos: Position) extends Message {
+      override def message: String =
+        s"State #$stateNo in mode $mode is inaccessible"
+
+    }
+
   }
 
 }
