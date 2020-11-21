3ce94093e9f2fac0a1f53bb98e8d06303167b3fe
Kalin-Rudnicki <deaddorks@gmail.com>
2020-11-20 21:19:15 -0600

    Basic test with argList

--- SlyceTest/src/test/scala/slyce/tests/argList/Run.scala
+++ SlyceTest/src/test/scala/slyce/tests/argList/Run.scala
@@ -11,8 +11,35 @@ object Run extends App {
     println(rawTree)
     println()
 
-  // TODO (KR) :
+    def loop(
+        unseen: NonTerminal.AnonList1_2,
+        seen: List[Token._var],
+    ): List[Token._var] =
+      unseen match {
+        case NonTerminal.AnonList1_2._1(_, _var, next) =>
+          loop(
+            next,
+            _var :: seen,
+          )
+        case NonTerminal.AnonList1_2._2 =>
+          seen.reverse
+      }
 
+    val vars: List[Token._var] =
+      rawTree match {
+        case NonTerminal.List._1(_, list, _) =>
+          list match {
+            case NonTerminal.AnonList1._1(_var, next) =>
+              loop(
+                next,
+                _var :: Nil,
+              )
+            case NonTerminal.AnonList1._2 =>
+              Nil
+          }
+      }
+
+    println(s"(${vars.map(_.text).mkString(", ")})")
   }
 
 }
